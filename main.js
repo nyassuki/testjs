759eff5e36ca5273c1c50fdc2c5ffd1129e9e0bb
nyassuki <77928925+nyassuki@users.noreply.github.com>
2025-01-31 13:48:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const { Buffer } = require('buffer');
-const bitcoin = require('bitcoinjs-lib');
-const ECPair = require('ecpair');
-const wif = require("wif");
-var CoinKey = require('coinkey');
+const CoinKey = require('coinkey');
 const fs = require('fs');
 const args = process.argv;
+const Jetty = require("jetty");
 
-
-let startF = args[2];
-
+const data = fs.readFileSync('progress.log',
+    { encoding: 'utf8', flag: 'r' });
+	 
+let startF = data;//args[2];
+var jetty = new Jetty(process.stdout);
 main(startF);
 
 
@@ -19,8 +19,14 @@ function main(startFrom) {
 		const one = BigInt(1);
 		// Create a buffer to pad our count to 32 bytes
 		const padded = Buffer.alloc(32);
+		
+		// Clear the screen
+		jetty.clear();
+			
+			
 		// Loop forever because we're never going to hit the end anyway
 		while (true) {
+			
 			// Increment our counter
 			count ++;
 			fs.writeFileSync('progress.log',"" + count);
@@ -33,12 +39,20 @@ function main(startFrom) {
 			var array = KeyAddress.split(":");
 			let PrivateKey = array[0];
 			let PublicAddress = array[1];
-			console.log(count + " -> " + padded + "," + PrivateKey + ","+PublicAddress);
+			
+			//console.log(count + " -> "  + "," + PrivateKey + ","+PublicAddress);
+			// write something
+			jetty.text(count + " -> "  + "," + PrivateKey + ","+PublicAddress);
+			jetty.moveTo([0,0]);
+			process.stdout.cursorTo(0);
 			if(PublicAddress == "1KTvsW5tg5gkJf9fyT2xsvjkv7dzuZNTpW" 
 			|| PublicAddress == "15ANYzzCp5BFHcCnVFzXqyibpzgPLWaD8b"
 			) {
 				process.stderr.write('\x07');
 				fs.appendFileSync('found/rns_found_balance2.txt', count + " -> " + PrivateKey + ","+PublicAddress);
+				break;
+			}
+			if(count==10000000) {
 				break;
 			}
 		}
